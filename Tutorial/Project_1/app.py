23bbf6d19fa8f6adf5b6b205ce46ea14923bace6
maryam <maryamosman0099@gmail.com>
2025-05-06 12:15:25 +0300

    Excersise_1

---
+++ Tutorial/Project_1/app.py
@@ -0,0 +1,34 @@
+# 1-Car Game
+'''
+command = ""
+while command.lower() != "quit":
+    command = input("> ").lower:
+    if command =="start":
+    print("Gooooooooo")
+    elif command == "stop":
+    print("Stopping")
+    elif command =="help":
+        print("""
+   start : to start the game
+   stop:to stop the game
+   quit to quit     
+        """        )
+'''
+#2-Using nested loops I need to draw this shape
+'''
+XXXXX
+XX
+xxxxx
+xx
+xx
+'''
+'''
+nums=[5,2,5,2,2]
+for x_count in nums:
+    output=''
+    for count in range(x_count):
+        output+='x'
+        print(output)
+'''
+
+
